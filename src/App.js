c6758847de15e620409a1b71d9a1c3d43a613b5c
igor <embiggenerd@gmail.com>
2019-12-16 12:39:39 -0500

    router links working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 const App = () => (
   <div>
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
     <Navigation />
   </div>
 );
